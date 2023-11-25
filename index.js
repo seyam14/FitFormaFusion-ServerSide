87be8d67a2cb0e1df06fca66fa0e7a10e1c71b63
seyam <mdseyam3215@gmail.com>
2023-11-25 20:14:08 +0600

    user api post and get

--- index.js
+++ index.js
@@ -10,6 +10,55 @@ app.use(cors());
 app.use(express.json());
 
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.izczxgs.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // user 
+    const userCollection = client.db('FitFormaFusionDB').collection('user');
+   
+
+    // user api
+    app.get('/user', async (req, res) => {
+        const cursor = userCollection.find();
+        const users = await cursor.toArray();
+        res.send(users);
+    })
+    
+    app.post('/user', async (req, res) => {
+        const user = req.body;
+        console.log(user);
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+    });
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 app.get('/', (req, res) => {
     res.send('server is running')
 })
