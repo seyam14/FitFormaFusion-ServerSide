1b492275b78a2314281e8e3b44c0d5e29be58a5d
seyam <mdseyam3215@gmail.com>
2023-11-26 04:01:15 +0600

    trainer get/:id

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.izczxgs.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -68,6 +68,13 @@ async function run() {
         const result = await trainerCollection.find().toArray();
         res.send(result);
     })
+
+    app.get('/trainer/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await trainerCollection.find().toArray(query);
+      res.send(result);
+  })
 
 
    
