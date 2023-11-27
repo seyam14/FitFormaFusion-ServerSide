973abfe36784d4ce0a7e40eaba2c1160d85f5e5b
seyam <mdseyam3215@gmail.com>
2023-11-27 23:43:55 +0600

    stripe

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 const port = process.env.PORT || 5000;
 
 
@@ -152,6 +154,25 @@ async function run() {
         const result = await userCollection.insertOne(user);
         res.send(result);
     });
+
+
+
+      // payment intent
+      app.post('/create-payment-intent', async (req, res) => {
+        const { price } = req.body;
+        const amount = parseInt(price * 100);
+        console.log(amount, 'amount inside the intent')
+
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: 'usd',
+          payment_method_types: ['card']
+        });
+
+        res.send({
+          clientSecret: paymentIntent.client_secret
+        })
+      });
 
 
     // Send a ping to confirm a successful connection
